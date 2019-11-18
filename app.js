f00d9604b6b3f2c841aa11a6b3656a9a7838771c
Borja Canseco <borj@cans.eco>
2019-11-18 00:22:08 -0600

    fix: auth error

--- app.js
+++ app.js
@@ -22,7 +22,10 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: true,
+}));
 app.use(session({ secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: false }));
 app.use(passport.initialize({userProperty: 'user'})); // req.user will contain the authenticated user object
 app.use(passport.session());
